b6fbc7ba30818fac200393862bb2e4637326be02
Grega Premoša <premgre@hilti.com>
2018-11-20 12:08:15 +0100

    fix v2

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component.ts'; //TODO: handle .ts
 import { SurveyComponent } from './survey/survey.component.ts'; //TODO: handle .ts
+import { AppComponent } from './app.component.ts'; //TODO: handle .ts
 
 @NgModule({
     imports: [BrowserModule],
